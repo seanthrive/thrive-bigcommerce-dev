86c3e2d0e636f9f30ac7ab3f5d7f374ac6150c52
sean <seanjcrl@gmail.com>
2020-11-09 16:36:04 -0500

    added dotenv for loading environment variables

--- app/controllers/omniauths_controller.rb
+++ app/controllers/omniauths_controller.rb
@@ -1,9 +1,25 @@
 class OmniauthsController < ApplicationController
   def callback
 
+    puts "------------ IN AUTH/:NAME/CALLBACK"
+
+    auth = request.env['omniauth.auth']
+    unless auth && auth[:extra][:raw_info][:context]
+      return render_error("[install] Invalid credentials: #{JSON.pretty_generate(auth[:extra])}")
+    end
+
+    email = auth[:info][:email]
+    store_hash = auth[:extra][:context].split('/')[1]
+    token = auth[:credentials][:token].token
+    scope = auth[:extra][:scopes]
+
+    # TODO: Find and/or create store and admin user.
+
+    redirect_to '/'
   end
 
   def load
+    render plain: "This is the load route"
   end
 
   def uninstall
