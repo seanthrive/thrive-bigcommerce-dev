4548f27abb228a58eb9bdb945717a4bf47717d93
sean <seanjcrl@gmail.com>
2020-11-16 12:08:29 -0500

    added store lookup to auth callback/install route

--- app/controllers/omniauths_controller.rb
+++ app/controllers/omniauths_controller.rb
@@ -1,19 +1,46 @@
 class OmniauthsController < ApplicationController
   def callback
 
-    puts "------------ IN AUTH/:NAME/CALLBACK"
-
     auth = request.env['omniauth.auth']
     unless auth && auth[:extra][:raw_info][:context]
       return render_error("[install] Invalid credentials: #{JSON.pretty_generate(auth[:extra])}")
     end
+
+    puts "[auth callback] Installing..."
 
     email = auth[:info][:email]
     store_hash = auth[:extra][:context].split('/')[1]
     token = auth[:credentials][:token].token
     scope = auth[:extra][:scopes]
 
-    # TODO: Find and/or create store and admin user.
+    # Lookup store
+    store = Store.find_by store_hash: store_hash
+
+    if store
+      # Update store record
+      puts "[auth callback] Updating token for store '#{store_hash}' with scope '#{scope}'"
+      store.update(access_token: token, scope: scope)
+      user = store.admin_user
+    else
+      # Create store record
+      puts "[auth callback] Installing app for store '#{store_hash}' and scope '#{scope}"
+      store = Store.create(store_hash: store_hash, access_token: token, scope: scope)
+
+      # Create admin user and associate with store
+      # user = User.first_or_create(email: email)
+      # user.stores << store
+      # user.save
+
+      # Set admin user in Store record
+      # store.admin_user_id = user.id
+      store.save
+    end
+
+    # Other one-time installation provisioning goes here.
+
+    # Login and redirect to home page
+    session[:store_id] = store.id
+    # session[:user_id] = user.id
 
     redirect_to '/'
   end
